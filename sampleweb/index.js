ef9db2eb2af932e405b56581f2a56884d2dc8f69
kumanan <kumananboss@gmail.com>
2024-02-05 14:57:08 +0530

    adding html file

--- sampleweb/index.js
+++ sampleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('${./}/ubuntu_test.html');
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(8080, () => {
